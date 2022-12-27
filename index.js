cf99672eb7f9bc92175b711b0c67b1fd1fd47566
Pedro Teixeira <40210465@esmad.ipp.pt>
2022-12-27 12:11:30 +0000

    pacman version 1

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ let canvas, scene, camera, renderer;
 // CREATE SCENE
  scene = new THREE.Scene();
 
-// Resize the canvas
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight,        
 }
 
+// Resize the canvas
 window.onresize = () =>{
     // to update the sizes
     sizes.width = window.innerWidth;
@@ -29,16 +29,6 @@ const loop = () => {
     renderer.render(scene, camera);
     window.requestAnimationFrame(loop);
 }
-
-// CREATE SHPERE => PACMAN
-let radius = 4;
-let width = 64;
-let height = 64;
-const geometryPacman = new THREE.SphereGeometry(radius, width, height);
-const materialPacman = new THREE.MeshStandardMaterial( {color: '#ffff00'} )
-const spherePacman = new THREE.Mesh(geometryPacman, materialPacman);
-scene.add(spherePacman);
-
 
 // ADD LIGHTS TO SCENE
 let color = 0xFFFFFF;
@@ -70,6 +60,57 @@ const controls = new orbitControls.OrbitControls(camera, canvas);
 // SET THE SIZE FOR THE CANVAS
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);  
+
+
+// PACMAN BODY
+const bodyGeometry = new THREE.CylinderGeometry(0.5, 0.5, 0.5, 32);
+const bodyMaterial = new THREE.MeshBasicMaterial({ color: 0xffd700 });
+const body = new THREE.Mesh(bodyGeometry, bodyMaterial);
+scene.add(body)
+
+// PACMAN EYES
+// LEFT EYE
+const eyeGeometry = new THREE.SphereGeometry(0.2, 32, 32);
+const eyeMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+const leftEye = new THREE.Mesh(eyeGeometry, eyeMaterial);
+leftEye.position.set(-0.15, 0.15, 0.25);
+scene.add(leftEye)
+
+const rightEye = new THREE.Mesh(eyeGeometry, eyeMaterial);
+rightEye.position.set(0.15, 0.15, 0.25);
+scene.add(rightEye)
+
+
+// MOUTH
+const mouthGeometry = new THREE.RingGeometry(0.55, 0.45, 32);
+const mouthMaterial = new THREE.MeshBasicMaterial({color: 0x000000});
+const mouth = new THREE.Mesh(mouthGeometry, mouthMaterial);
+mouth.rotation.x = Math.PI / 2;
+scene.add(mouth);
+
+let angle = 0;
+
+function updatePacman() {
+    // Update the angle of Pac-Man
+    angle += 0.01;
+  
+    // Update the position of Pac-Man's eyes to follow the mouse
+    leftEye.position.x = -0.15 + Math.sin(angle) * 0.1;
+    leftEye.position.y = 0.15 + Math.cos(angle) * 0.1;
+    rightEye.position.x = 0.15 + Math.sin(angle) * 0.1;
+    rightEye.position.y = 0.15 + Math.cos(angle) * 0.1;
+  
+    // Animate the mouth of Pac-Man
+    mouth.scale.y = Math.sin(angle) + 1.5;
+  }
+  
+
+// Call the updatePacman function on each frame
+function animate() {
+    requestAnimationFrame(animate);
+    updatePacman();
+  }
+  animate();
 
 
 loop();
