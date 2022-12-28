1942de4fbddc506a61c5aea7eddd9e83aca99f01
Pedro Teixeira <40210465@esmad.ipp.pt>
2022-12-28 19:07:21 +0000

    pacman v2.1 update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as THREE from './libs/three.module.js'
-import * as orbitControls from './libs/OrbitControls.js'
+import * as OrbitControls from './libs/OrbitControls.js'
 
 // global variables
 let canvas, scene, camera, renderer;
@@ -7,6 +7,7 @@ let canvas, scene, camera, renderer;
 // CREATE SCENE
  scene = new THREE.Scene();
 
+// Object for the size of the window
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight,        
@@ -17,14 +18,14 @@ window.onresize = () =>{
     // to update the sizes
     sizes.width = window.innerWidth;
     sizes.height = window.innerHeight;
-    // to update the camera
+    // to update the camera projection 
     camera.updateProjectionMatrix();
     camera.aspect = sizes.width / sizes.height;
     renderer.setSize(sizes.width, sizes.height);
     // console.log(sizes.width, sizes.height);
 }
 
-// arrow function loop
+// arrow function loop to rezise
 const loop = () => {
     renderer.render(scene, camera);
     window.requestAnimationFrame(loop);
@@ -42,10 +43,11 @@ light.position.set(0, 0, 20);
 scene.add(light);
 
 // ADD THE CAMERA
-let FOV = 45; //field of view of the camera
+let FOV = 100; //field of view of the camera
+let aspectRatio = sizes.width / sizes.height;
 let near = 0.1;
 let far = 100;
- camera = new THREE.PerspectiveCamera(FOV, sizes.width / sizes.height, near, far);
+ camera = new THREE.PerspectiveCamera(FOV, aspectRatio , near, far);
 // set position for the camera
 camera.position.z = 20; 
 scene.add(camera);
@@ -55,62 +57,87 @@ scene.add(camera);
  renderer = new THREE.WebGLRenderer({canvas});
 
 //TO CONTROL THE CAMERA WITH ORBIT_CONTROLS
-const controls = new orbitControls.OrbitControls(camera, canvas);
+const controls = new OrbitControls.OrbitControls(camera, canvas);
 
 // SET THE SIZE FOR THE CANVAS
-renderer.setSize(sizes.width, sizes.height);
+let renderWidth = sizes.width;
+let renderHeigth = sizes.height;
+renderer.setSize(renderWidth, renderHeigth);
 renderer.render(scene, camera);  
 
+// AXES HELPER
+let size = 45;
+const axesHelper = new THREE.AxesHelper( size );
+// scene.add( axesHelper );
 
-// PACMAN BODY
-const bodyGeometry = new THREE.CylinderGeometry(0.5, 0.5, 0.5, 32);
-const bodyMaterial = new THREE.MeshBasicMaterial({ color: 0xffd700 });
-const body = new THREE.Mesh(bodyGeometry, bodyMaterial);
-scene.add(body)
+// BODY
+// B = Body
+let Bradius = 8;
+let Bwidth = 32;
+let Bheight = 32;
+let BphiStart = 0;
+let BphiLength = Math.PI * 2;
+let BthetaStart = 0;
+let BthetaLength = Math.PI / 2;
 
-// PACMAN EYES
-// LEFT EYE
-const eyeGeometry = new THREE.SphereGeometry(0.2, 32, 32);
-const eyeMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
-const leftEye = new THREE.Mesh(eyeGeometry, eyeMaterial);
-leftEye.position.set(-0.15, 0.15, 0.25);
-scene.add(leftEye)
+const headGeometry = new THREE.SphereGeometry(Bradius, Bwidth, Bheight, BphiStart, BphiLength, BthetaStart, BthetaLength);
+const headMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });                 
+const head1 = new THREE.Mesh(headGeometry, headMaterial);
+head1.position.set(-0, 5, -5)
+scene.add(head1);
 
-const rightEye = new THREE.Mesh(eyeGeometry, eyeMaterial);
-rightEye.position.set(0.15, 0.15, 0.25);
-scene.add(rightEye)
-
+const headGeometry2 = new THREE.SphereGeometry(Bradius, Bwidth, Bheight, BphiStart, BphiLength, BthetaStart, BthetaLength);
+const head2 = new THREE.Mesh(headGeometry2, headMaterial);
+head2.position.set(-0, 5, -5)
+head2.rotation.x = Math.PI / true;
+scene.add(head2);
+head1.rotateX(THREE.Math.degToRad(-15));
+head2.rotateX(THREE.Math.degToRad(25));
 
 // MOUTH
-const mouthGeometry = new THREE.RingGeometry(0.55, 0.45, 32);
-const mouthMaterial = new THREE.MeshBasicMaterial({color: 0x000000});
+let Mradius = 8;
+let Mwidth = 32;
+let Mheight = 32;
+let MphiStart = 0;
+let MphiLength = Math.PI * 2;
+let MthetaStart = 0;
+let MthetaLength = Math.PI / 2;
+
+const mouthGeometry = new THREE.SphereGeometry(Mradius, Mwidth, Mheight, MphiStart, MphiLength, MthetaStart, MthetaLength);
+const mouthMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
 const mouth = new THREE.Mesh(mouthGeometry, mouthMaterial);
-mouth.rotation.x = Math.PI / 2;
+mouth.position.set(-0, 5, -5);
+mouth.rotation.x = Math.PI / true;
 scene.add(mouth);
+mouth.rotateX(THREE.Math.degToRad(-5));
 
-let angle = 0;
+// EYES
+let Eyeradius = 1;
+let Eyewidth = 12;
+let Eyeheight = 12;
 
-function updatePacman() {
-    // Update the angle of Pac-Man
-    angle += 0.01;
-  
-    // Update the position of Pac-Man's eyes to follow the mouse
-    leftEye.position.x = -0.15 + Math.sin(angle) * 0.1;
-    leftEye.position.y = 0.15 + Math.cos(angle) * 0.1;
-    rightEye.position.x = 0.15 + Math.sin(angle) * 0.1;
-    rightEye.position.y = 0.15 + Math.cos(angle) * 0.1;
-  
-    // Animate the mouth of Pac-Man
-    mouth.scale.y = Math.sin(angle) + 1.5;
-  }
-  
+// LEFT EYE
+const EyeGeometry = new THREE.SphereGeometry(Eyeradius, Eyewidth, Eyeheight);
+const EyeMaterial = new THREE.MeshBasicMaterial({color: 0x0000 });
+const leftEye = new THREE.Mesh(EyeGeometry, EyeMaterial);
+leftEye.position.set(-2, 9, 1);
+scene.add(leftEye)
+// leftEye.add(axesHelper)
 
-// Call the updatePacman function on each frame
+
+// RIGHT EYE
+const rightEye = new THREE.Mesh(EyeGeometry, EyeMaterial);
+rightEye.position.set(2, 9, 1);
+scene.add(rightEye)
+
 function animate() {
-    requestAnimationFrame(animate);
-    updatePacman();
-  }
-  animate();
 
+    requestAnimationFrame(animate)
 
+    mouth.rotation.x -= 0.05;
+
+    renderer.render(scene, camera);
+}
+
+animate();
 loop();
